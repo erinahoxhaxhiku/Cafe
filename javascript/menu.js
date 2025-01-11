d97699d7f8200fc2f6afbd1c5f914ca6dfed77be
argita00 <aargitakurtishaj@gmail.com>
2025-01-11 10:10:56 +0100

    hover txt tooltip

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,3 +1,35 @@
-function confirmOrder(){
-    document.getElementById ('orderConfirmation').style.display = "block";
-}
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.classList.add("fade-in"); 
+  });
+  
+ 
+
+document.querySelectorAll('.fourth-part .row .col img').forEach(img => {
+    let tooltipTimeout;
+    const tooltip = document.createElement('div');
+    tooltip.classList.add('tooltip'); 
+    document.body.appendChild(tooltip); 
+  
+    img.addEventListener('mouseenter', function() {
+      tooltip.textContent = img.getAttribute('data-tooltip'); 
+  
+      
+      tooltipTimeout = setTimeout(() => {
+        tooltip.style.visibility = 'visible';
+        tooltip.style.opacity = '1'; 
+        const rect = img.getBoundingClientRect();
+        tooltip.style.top = rect.top - tooltip.offsetHeight - 10 + 'px'; 
+        tooltip.style.left = rect.left + (rect.width / 2) - (tooltip.offsetWidth / 2) + 'px'; 
+      }, 300); 
+    });
+  
+    img.addEventListener('mouseleave', function() {
+      clearTimeout(tooltipTimeout); 
+      
+      setTimeout(() => {
+        tooltip.style.visibility = 'hidden'; 
+      }, 300); 
+    });
+  });
+  
+  
