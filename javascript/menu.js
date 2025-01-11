625fcc10f17a4f63662ab09da4825382b771fe6b
argita00 <aargitakurtishaj@gmail.com>
2025-01-11 12:02:07 +0100

    .

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,19 +1,19 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const elements = document.querySelectorAll('.hidden');
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.querySelectorAll('.hidden');
   
-//     const observer = new IntersectionObserver((entries, observer) => {
-//       entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//           entry.target.classList.add('visible');
-//           observer.unobserve(entry.target); // Stop observing after animation
-//         }
-//       });
-//     }, {
-//       threshold: 0.1, // Trigger when 10% of the element is visible
-//     });
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target); // Stop observing after animation
+        }
+      });
+    }, {
+      threshold: 0.1, // Trigger when 10% of the element is visible
+    });
   
-//     elements.forEach(element => {
-//       observer.observe(element);
-//     });
-//   });
+    elements.forEach(element => {
+      observer.observe(element);
+    });
+  });
   
